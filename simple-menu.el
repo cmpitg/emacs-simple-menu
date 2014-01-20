a42a702ee6f3875b76204230bb447e4686fb7efd
Duong H. Nguyen <cmpitg@gmail.com>
2014-01-20 16:22:25 +0700

    Library skeleton

---
+++ simple-menu.el
@@ -0,0 +1,28 @@
+;;; simple-menu.el --- Simple way to build menu
+
+;; Copyright (C) 2014  cmpitg
+
+;; Author: cmpitg <cmpitg@melec>
+;; Keywords: extensions
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; simple-menu is a library to simplify how users could build menu.
+
+;;; Code:
+
+(provide 'simple-menu)
+;;; simple-menu.el ends here
